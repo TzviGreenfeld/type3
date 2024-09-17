27807dacae77297311ee1db08273cf4faaa63ec6
omerb97 <omerb97@gmail.com>
2024-09-17 14:49:43 +0300

    add navigation button

--- type3-app/app/LinkingPage.tsx
+++ type3-app/app/LinkingPage.tsx
@@ -1,5 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { CallButton, WhatsappButton, SMSButton } from "@/components/ContactButtons";
+import { CallButton, WhatsappButton, SMSButton, OpenMapButton } from "@/components/ContactButtons";
 import { StyleSheet } from "react-native";
 import { View } from "react-native";
 import SmallMapOverview from "@/components/SmallMapOverview";
@@ -9,7 +9,12 @@ const LinkingPage = () => {
     return (
         <SafeAreaView style={styles.container}>
             <LinkNameLabel name="Israel Israeli" />
-            <SmallMapOverview askerPoint={{ latitude: 32.0853, longitude: 34.7818 }} donorPoint={{ latitude: 31.0853, longitude: 34.7818 }} />
+            <View style={styles.mapContainer}>
+                <SmallMapOverview askerPoint={{ latitude: 32.0853, longitude: 34.7818 }} donorPoint={{ latitude: 31.0853, longitude: 34.7818 }} />
+            </View>
+            <View style={styles.buttonContainer}>
+                <OpenMapButton start={{ latitude: 32.0853, longitude: 34.7818 }} end={{ latitude: 31.0853, longitude: 34.7818 }} />
+            </View>
             <View style={styles.buttonContainer}>
                 <CallButton phoneNumber="1234567890" />
             </View>
@@ -32,6 +37,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     buttonContainer: {
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    mapContainer: {
         marginTop: 30,
+        marginBottom: 30,
     },
 });
