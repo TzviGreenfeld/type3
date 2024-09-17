36fcd739c2d0d3d8f3f0568bd680a57945dcf39c
Tzvi Greenfeld <tzvigreenfield@gmail.com>
2024-09-17 15:54:11 +0300

    fix users

--- type3-app/app/index.tsx
+++ type3-app/app/index.tsx
@@ -1,10 +1,6 @@
-import { useEffect, useState } from "react";
 import { Redirect } from "expo-router";
-import { usePushNotifications } from "@/hooks/usePushNotifications";
-import { Text } from "react-native-paper";
 
 export default function Index() {
-  const [users, setUsers] = useState<User[]>([]);
 
   return (
 
