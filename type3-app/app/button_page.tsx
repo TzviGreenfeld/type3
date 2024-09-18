659381517cc69f58e58821a59709ebbe1a9306bc
omerb97 <omerb97@gmail.com>
2024-09-18 19:24:14 +0300

    beutify the circle button

--- type3-app/app/button_page.tsx
+++ type3-app/app/button_page.tsx
@@ -1,10 +1,11 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import CircleButton from "../components/CircleButton";
 import { StyleSheet } from "react-native";
-
+import { Text } from "react-native-paper";
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
+        <Text style={styles.sosText}>SOS{'\n'}Insulin!</Text>
         <CircleButton />
     </SafeAreaView>
   );
@@ -15,6 +16,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    
+    backgroundColor: '#385399',
+  },
+  sosText: {
+    fontSize: 60,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+    marginBottom: 20,
   },
 });
