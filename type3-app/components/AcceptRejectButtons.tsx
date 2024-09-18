a3f114363f190fc4f54eb8c56c39f1b02aa7fd60
Tzvi Greenfeld <tzvigreenfield@gmail.com>
2024-09-18 20:30:03 +0300

    onreject

--- type3-app/components/AcceptRejectButtons.tsx
+++ type3-app/components/AcceptRejectButtons.tsx
@@ -5,57 +5,70 @@ import { useRouter } from "expo-router";
 import { completeRequest } from "@/scripts/requestService";
 
 const onReject = () => {
-    const router = useRouter();
-    console.log("Reject");
-    router.replace('/button_page');
-}
+  const router = useRouter();
+  console.log("Reject");
+  router.replace("/button_page");
+};
+
 const onAccept = async (requestId: string) => {
-    console.log("Accept");
-    const response = await completeRequest(requestId);  
-    console.log(response);
-    const router = useRouter();
-    router.push({
-    pathname: '/LinkingPage',
-    params: { resultJson: JSON.stringify(response.data), showType: "response"}
-    });
-}
+  console.log("Accept");
+  const response = await completeRequest(requestId);
+  console.log(response);
+  const router = useRouter();
+  router.push({
+    pathname: "/LinkingPage",
+    params: { resultJson: JSON.stringify(response.data), showType: "response" },
+  });
+};
+
 const AcceptRejectButtons = ({ requestId }: { requestId: string }) => {
-    return (
-            <View style={styles.buttonContainer}>
-                <Button mode="contained" onPress={() => onAccept(requestId)} style={styles.acceptButton}>Accept</Button>
-                <Button mode="contained" onPress={onReject} style={styles.rejectButton}>Reject</Button>
-            </View>
-    );
-}
+  return (
+    <View style={styles.buttonContainer}>
+      <Button
+        mode="contained"
+        onPress={() => onAccept(requestId)}
+        style={styles.acceptButton}
+      >
+        Accept
+      </Button>
+      <Button
+        mode="contained"
+        onPress={() => onReject()}
+        style={styles.rejectButton}
+      >
+        Reject
+      </Button>
+    </View>
+  );
+};
 
 export default AcceptRejectButtons;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
-        },
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '80%',
-        alignItems: 'center',
-        marginTop: 20,
-    },
-    acceptButton: {
-        width: '48%',
-        borderRadius: 20,
-        aspectRatio: 1,
-        backgroundColor: 'green',
-        justifyContent: 'center',
-    },
-    rejectButton: {
-        width: '48%',
-        borderRadius: 20,
-        aspectRatio: 1,
-        backgroundColor: 'red',
-        justifyContent: 'center',
-    },
-
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  acceptButton: {
+    width: "48%",
+    borderRadius: 20,
+    aspectRatio: 1,
+    backgroundColor: "green",
+    justifyContent: "center",
+  },
+  rejectButton: {
+    width: "48%",
+    borderRadius: 20,
+    aspectRatio: 1,
+    backgroundColor: "red",
+    justifyContent: "center",
+  },
 });
