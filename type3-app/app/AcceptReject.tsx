714604c426e245ac52fd6ee1853d33b9b59bf5db
Tzvi Greenfeld <tzvigreenfield@gmail.com>
2024-09-18 17:45:16 +0300

    requestid in acceptreject

--- type3-app/app/AcceptReject.tsx
+++ type3-app/app/AcceptReject.tsx
@@ -4,31 +4,43 @@ import { StyleSheet } from "react-native";
 import SmallMapOverview from "@/components/SmallMapOverview";
 import AcceptRejectLabel from "@/components/AcceptRejectLabel";
 import { View } from "react-native";
+import { RouteProp, useRoute } from "@react-navigation/native";
+
+type LinkingPageRouteProp = RouteProp<
+  { params: { requestId: string } },
+  "params"
+>;
 const AcceptReject = () => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <AcceptRejectLabel name="Tzvi Greenfeld" />
-            <View style={styles.innerContainer}>
-                <SmallMapOverview selfPoint={{ latitude: 32.0853, longitude: 34.7818 }} otherPoint={{ latitude: 31.0853, longitude: 34.7818 }} />
-                <AcceptRejectButtons />
-            </View>
-        </SafeAreaView>
-    );
-}
+  const route = useRoute<LinkingPageRouteProp>();
+  const requestId = route.params.requestId;
+  console.log("requestId from AcceptReject", requestId);
+  return (
+    <SafeAreaView style={styles.container}>
+      <AcceptRejectLabel name="Tzvi Greenfeld" />
+      <View style={styles.innerContainer}>
+        <SmallMapOverview
+          selfPoint={{ latitude: 32.0853, longitude: 34.7818 }}
+          otherPoint={{ latitude: 31.0853, longitude: 34.7818 }}
+        />
+        <AcceptRejectButtons />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    innerContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
-        marginBottom: 20,
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    marginBottom: 20,
+  },
 });
 
 export default AcceptReject;
