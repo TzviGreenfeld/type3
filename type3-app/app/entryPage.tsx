6d7db057993aee9071b0af7d922186dbdb24952b
Tzvi Greenfeld <tzvigreenfield@gmail.com>
2024-09-18 18:18:40 +0300

    hook in entry as well

--- type3-app/app/entryPage.tsx
+++ type3-app/app/entryPage.tsx
@@ -1,28 +1,30 @@
-import { useRouter } from 'expo-router';
-import React, { useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { usePushNotifications } from "@/hooks/usePushNotifications";
 
 const EntryPage = () => {
-    const router = useRouter();
-    console.log('EntryPage');
-    useEffect(() => {
-        const checkToken = async () => {
-            
-            const token = await AsyncStorage.getItem('token');
-            console.log('Token:', token);
-            if (token) {
-                router.replace('/button_page');
-            } else {
-                router.replace('/registration');
-            }
-        };
-        checkToken();
-    }, [router]);
+  const { expoPushToken, notification } = usePushNotifications();
 
-    return null;
+  const router = useRouter();
+  console.log("EntryPage");
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("token");
+      console.log("Token:", token);
+      if (token) {
+        router.replace("/button_page");
+      } else {
+        router.replace("/registration");
+      }
+    };
+    checkToken();
+  }, [router]);
+
+  return null;
 };
 EntryPage.navigationOptions = {
-    headerShown: false,
+  headerShown: false,
 };
 
 export default EntryPage;
